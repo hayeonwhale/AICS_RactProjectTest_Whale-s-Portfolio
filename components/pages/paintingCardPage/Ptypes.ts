301599ec4300cdd8a94c1c2acd9c7ad82eb9d016
유저이름 < >
2025-12-26 05:35:38 +0900

    update homepage

--- components/pages/paintingCardPage/Ptypes.ts
+++ components/pages/paintingCardPage/Ptypes.ts
@@ -12,17 +12,9 @@ export interface PosterContent {
   tags: string[];
 }
 
-// export interface PosterData {
-//   imageUrl: string;
-//   content: {
-//     title: string;
-//     artist: string;
-//     year: string;
-//     intro: string;
-//     pastTheme: string;
-//     modernTheme: string;
-//     connection: string;
-//     coreMessage: string;
-//     tags: string[];
-//   };
-// }
+// 👇 [핵심] PosterData도 추가해서 에러 해결!
+export interface PosterData {
+  id: number;
+  imageUrl: string;
+  content: PosterContent;
+}
